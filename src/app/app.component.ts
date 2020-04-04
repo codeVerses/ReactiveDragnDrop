38c1ab7d3535cce750554898b44cc6f33f00ec16
Janusz Cimek <info@jcimek.co.uk>
2020-04-04 19:14:33 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ interface positionInt {
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title = "Drag and Drop";
